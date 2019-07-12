b24bd4c165ae6cdb938a2bd3ee4d33707a313752
appleat <appleat@live.com>
2019-07-12 14:17:04 +0800

    add view demo

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,7 +3,62 @@ import 'package:flutter/material.dart';
 import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridViewBuilderDemo();
+  }
+}
 
+class GridViewBuilderDemo extends StatelessWidget {
+  Widget _gridviewBuilder(BuildContext context, int index) {
+    return Container(
+      child: Image.network(
+        posts[index].url,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.all(8),
+      itemCount: posts.length,
+      itemBuilder: _gridviewBuilder,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 2 / 3,
+      ),
+    );
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0, 0),
+        child: Text("item $index"),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 150,
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+//      scrollDirection: Axis.horizontal, // 横向滚动
+      children: _buildTiles(54),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
   List<Widget> _buildTiles(int length) {
     return List.generate(length, (int index) {
       return Container(
@@ -21,8 +76,7 @@ class ViewDemo extends StatelessWidget {
       crossAxisSpacing: 16,
       mainAxisSpacing: 16,
 //      scrollDirection: Axis.horizontal, // 横向滚动
-      children: _buildTiles(50)
-      ],
+      children: _buildTiles(54),
     );
   }
 }
@@ -32,8 +86,7 @@ class PageViewBuilderDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return PageView.builder(
         itemCount: posts.length,
-        itemBuilder: (context, index) =>
-            Stack(
+        itemBuilder: (context, index) => Stack(
               children: <Widget>[
                 SizedBox.expand(
                   child: Image.network(
